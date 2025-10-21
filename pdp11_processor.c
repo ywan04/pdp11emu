36eb6304c5339b2ac99cb0fd1d5e9054f9000a8e
Ivan Bereza <iv.bereza04@gmail.com>
2025-10-21 18:18:48 +0300

    fixed swaped operands

--- pdp11_processor.c
+++ pdp11_processor.c
@@ -163,7 +163,7 @@ uint8_t get_src(uint16_t *adr)
 {
 	uint8_t ss;
 
-	ss = (uint8_t)(curins & 0000077);
+	ss = (uint8_t)((curins & 0007700) >> 6);
 
 	return parse_arg(adr, ss, 2);
 }
@@ -172,7 +172,7 @@ uint8_t get_dst(uint16_t *adr)
 {
 	uint8_t dd;
 	
-	dd = (uint8_t)((curins & 0007700) >> 6);
+	dd = (uint8_t)(curins & 0000077);
 
 	return parse_arg(adr, dd, 2);
 }
@@ -181,7 +181,7 @@ uint8_t get_srcb(uint16_t *adr)
 {
 	uint8_t ss;
 
-	ss = (uint8_t)(curins & 0000077);
+	ss = (uint8_t)((curins & 0007700) >> 6);
 
 	return parse_arg(adr, ss, 1);
 }
@@ -190,7 +190,7 @@ uint8_t get_dstb(uint16_t *adr)
 {
 	uint8_t dd;
 	
-	dd = (uint8_t)((curins & 0007700) >> 6);
+	dd = (uint8_t)(curins & 0000077);
 
 	return parse_arg(adr, dd, 1);
 }
