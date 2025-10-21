5f718acd4b4d30eb918a14b0194606f5ace3c7ed
Ivan Bereza <iv.bereza04@gmail.com>
2025-10-21 23:57:33 +0300

    inited moving to ncurses

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ VERSION = 0.1
 # flags
 #CFLAGS  = -std=c99 -pedantic -Wall -O2 -DVERSION=\"${VERSION}\"
 CFLAGS  = -g -std=c99 -pedantic -Wall -O0 -DVERSION=\"${VERSION}\" -DDEBUG_ENABLE
-LDFLAGS =
+LDFLAGS = -lncurses
 
 # compiler and linker
 CC = cc
--- main.c
+++ main.c
@@ -1,34 +1,19 @@
 #include "pdp11_memory.h"
 #include "pdp11_processor.h"
 
+#include <ncurses.h>
+
 #include <assert.h>
-
-void mem_test(void)
-{
-	uint8_t a, b;
-	uint16_t w;
-
-	writew(0, 0x5498);
-	a = readb(0);
-	b = readb(1);
-	assert(a == 0x98);
-	assert(b == 0x54);
-
-	writeb(1, 0x23);
-	b = readb(1);
-	assert(a == 0x98);
-	assert(b == 0x23);
-
-	w = readw(0);
-	assert(w == 0x2398);
-}
 
 int main(int argc, char *argv[])
 {
-	mem_test();
-	
-	loadfile(argv[1]);
-	run();
+	initscr();
+
+	//loadfile(argv[1]);
+	//run();
+
+	getch();
+	endwin();
 
 	return 0;
 }
