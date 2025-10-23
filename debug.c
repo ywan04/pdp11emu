2698a6f18028a6d2e1bc9bcd088d07c2d96a7729
Ivan Bereza <iv.bereza04@gmail.com>
2025-10-23 12:51:58 +0300

    debug window

--- debug.c
+++ debug.c
@@ -1,21 +1,66 @@
-#ifndef DEBUG_ENABLE
+#include "debug.h"
+#include "system.h"
 
-void trace(const char *format, ...)
-{
-}
-
-#else // DEBUG_ENABLE
+#include <ncurses.h>
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdint.h>
 
-void trace(const char *format, ...)
+static WINDOW *win;
+
+void debug_create(void)
+{
+	if (win != NULL) {
+		system_exit("error: debug window is already created\n",
+			    SYSTEM_ERROR);
+	}
+	
+	win = newwin(DEBUG_HEIGHT, DEBUG_WIDTH, DEBUG_Y, DEBUG_X);
+	box(debug, 0, 0);
+	mvwprintw(debug, 0, 2, "debug");
+}
+
+void debug_destroy(void)
+{
+	if (win != NULL) {
+		delwin(win);
+		win = NULL;
+	} else {
+		system_exit("error: debug window is clean\n", SYSTEM_ERROR);
+	}
+}
+
+void debug_silent_destroy(void)
+{
+	if (win != NULL) {
+		delwin(win);
+		win = NULL;
+	}
+}
+
+void debug_print_regs(uint16_t *reg)
+{
+	int i;
+
+	for (i = 0; i < 8; i += 2) {
+		mvwprintw(win, i+2, (DEBUG_WIDTH-2*(4+6))/3, "R%d: %06o");
+	}
+	for (i = 1; i < 8; i += 2) {
+		mvwprintw(win, i+2, 2*(DEBUG_WIDTH-(4+6))/3, "R%d: %06o");
+	}
+}
+
+void debug_print_init(void)
+{
+	wmove(win, DEBUG_HEIGHT-3, 7);
+}
+
+void debug_print(const char *format, ...)
 {
 	va_list args;
 	
 	va_start(args, format);
-	vprintf(format, args);
+	wvprintw(win, format, args);
 	va_end(args);
 }
-
-#endif // DEBUG_ENABLE
--- debug.h
+++ debug.h
@@ -1,6 +1,17 @@
 #ifndef DEBUG__H
 #define DEBUG__H
 
-void trace(const char *format, ...);
+#define DEBUG_Y (LINES/2)
+#define DEBUG_X 0
+
+#define DEBUG_HEIGHT (LINES/2)
+#define DEBUG_WIDTH (COLS/2)
+
+void debug_create(void);
+void debug_destroy(void);
+void debug_silent_destroy(void);
+void debug_print_regs(uint16_t *reg);
+void debug_print_init(void);
+void debug_print(void);
 
 #endif // DEBUG__H
