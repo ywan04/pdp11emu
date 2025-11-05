3056215c1bbfba5e8f4c36e9b725c0b20b7de951
Ivan Bereza <iv.bereza04@gmail.com>
2025-11-05 18:33:48 +0200

    options

--- main.c
+++ main.c
@@ -3,21 +3,63 @@
 
 #include "debug.h"
 #include "terminal.h"
+#include "system.h"
 
 #include <ncurses.h>
 
-#include <assert.h>
+#include <stdio.h>
+
+void help(const char *progname)
+{
+	printf("usage: %s options [arguments]\n", progname);
+	// todo: description for every option
+}
 
 int main(int argc, char *argv[])
 {
+	int i, j;
+
+	if (argc == 1) {
+		help(argv[0]);
+		return SYSTEM_ERROR;
+	}
+
+	for (i = 0, j = 2; argv[1][i] != '\0'; ++i) {
+		switch (argv[1][i]) {
+		case 'h':
+			help(argv[0]);
+			return SYSTEM_OK;
+		case 'v':
+			printf("%s-"VERSION"\n", argv[0]);
+			return SYSTEM_OK;
+		case 'f':
+			if (j >= argc) {
+				fprintf(stderr, "error: not enough arguments\n");
+				return SYSTEM_ERROR;
+			}
+			loadfile(argv[j++]);
+			break;
+		case 'd':
+		case 'D':
+			fprintf(stderr, "error: not implemented\n");
+			return SYSTEM_ERROR;
+		default:
+			fprintf(stderr,
+				"error: unknown option %c\n", argv[1][i]);
+			return SYSTEM_ERROR;
+		}
+	}
+
+	if (j < argc) {
+		fprintf(stderr, "error: too many arguments\n");
+		return SYSTEM_ERROR;
+	}
+
 	initscr();
 
 	debug_create();
 	terminal_create();
 
-	if (argc == 2) {
-		loadfile(argv[1]);
-	}
 	run();
 
 	terminal_destroy();
@@ -25,5 +67,5 @@ int main(int argc, char *argv[])
 
 	endwin();
 
-	return 0;
+	return SYSTEM_OK;
 }
